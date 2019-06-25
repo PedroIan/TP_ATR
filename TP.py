a6461f3263765cae6781e23fc71aaf1b125400c4
Pedro Ian <pedroianmoraesus@gmail.com>
2019-06-25 01:47:27 -0300

    Update TP.py
    Funfando e com auxílio socket

--- TP.py
+++ TP.py
@@ -4,6 +4,7 @@ from random import uniform as uni
 import socket
 import threading
 from threading import Semaphore as sem
+from threading import Lock as lock
 from threading import Thread as thr
 import numpy as np
 from numpy import pi as pi
@@ -13,9 +14,9 @@ try:
 except:
     os.system('python -m pip install scipy')
 
-sema = sem()
+sema = lock()
 maxIn = 10**(1/2)
-qin = 7
+qin = maxIn
 qout = 0
 h = []
 h.append(0)
@@ -27,12 +28,24 @@ H = 10
 
 class process_thread(thr):
     def __init__(self):
+        self.endCli = clientAddress
+        self.sockCli = clientsocket
         thr.__init__(self)
 
     def run(self):
         while True:
+            global sema
+            global maxIn
+            global qin
+            global qout
+            global h
+            global href
+            global R0
+            global R1
+            global H
             sema.acquire()
             qout = uni(0.5, 1)*(h[-1]**(1/2))
+            #print("qout - ", qout)
             h.append(self.RungeKuttaSimples())
             sema.release()
             time.sleep(0.05)
@@ -54,6 +67,7 @@ class process_thread(thr):
 
     def RungeKuttaSimples(self):
         try:
+            #print(qin, qout, self.volumeC(h[-1]))
             return (qin - qout + self.volumeC(h[-1]))/(pi*(R0**2))
         except:
             pass
@@ -86,36 +100,58 @@ class process_thread(thr):
 class softPLC_thread(thr):
     def __init__(self):
         self.process = process_thread()
+        self.endCli = clientAddress
+        self.sockCli = clientsocket
         thr.__init__(self)
         pass
 
     def run(self):
         while True:
+            global sema
+            global maxIn
+            global qin
+            global qout
+            global h
+            global href
+            global R0
+            global R1
+            global H
             sema.acquire()
             nexth = self.process.RungeKuttaSimples()
             if href < H:
                 nextIn = self.process.volumeC(href) - self.process.volumeC(h[-1]) + qout
-                print(h[-1])
+                #print("h atual - ", h[-1])
                 if nextIn > maxIn:
                     qin = maxIn
                 elif nextIn < 0:
                     qin = 0
                 else:
                     qin = nextIn
-                print(qin)
+                #print("qin atual -", qin)
             else:
                 qin = self.process.volumeC(H) - self.process.volumeC(h[-1]) + qout
+            self.sockCli.send(byte(qin), byte(qout), byte(h[-1]))
             sema.release()
             time.sleep(0.1)
         self._stop()
 
+LOCALHOST = "127.0.0.1"
+PORT = 50000
 
+#Conexão TCP/IP preparação para conexão cliente servidor (lado servidor)
+
+print("Inicializando servidor de simulação....")
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server.bind((LOCALHOST, PORT))
+server.listen(1)
+clientsock, clientAddress = server.accept()
 
 
 href = int(input())
-pro = process_thread()
+pro = process_thread(clientsock, clientAddress)
 pro.start()
-plc = softPLC_thread()
+plc = softPLC_thread(clientsock, clientAddress)
 plc.start()
 time.sleep(5)
 for i in range(len(h)):
